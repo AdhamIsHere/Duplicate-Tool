6af9960726f56a7c509d2c1dad23f3f142b73b0f
Adham Aboelela <100962253+AdhamIsHere@users.noreply.github.com>
2025-01-31 21:38:07 +0200

    Update Google Java Format dependency in setup.py and SOURCES.txt

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     packages=find_packages(),
     package_data={
-        "Duplicate_Tool": ["resources/google-java-format.jar"],
+        "Duplicate_Tool": ["resources/google-java-format-1.25.2-all-deps.jar"],
     },
     include_package_data=True,
     install_requires=[
